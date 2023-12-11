50f2974ab188914b74d684cf01d8f6af61460abf
Shine Sujith <G00414454@atu.ie>
2023-12-11 16:31:42 +0000

    class Trawler was created

---
+++ src/main/java/ie/atu/Trawler.java
@@ -0,0 +1,30 @@
+package ie.atu;
+
+public class Trawler extends Ship{
+    private int cargo;
+
+    public Trawler() {
+        this.cargo = 0;
+    }
+
+    public Trawler(int cargo) {
+        this.cargo = cargo;
+    }
+
+    public Trawler(String name, String year, int crew, int cargo) {
+        super(name, year, crew);
+        this.cargo = cargo;
+    }
+
+    public int getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(int cargo) {
+        this.cargo = cargo;
+    }
+
+    public void vesselDetails() {
+        System.out.println("ships name: " + super.getName() + "cargo in tonnage: " + getCargo());
+    }
+}
